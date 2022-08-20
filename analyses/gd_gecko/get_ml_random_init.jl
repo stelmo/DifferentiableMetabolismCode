f4f7b0c733be19527365ff659d07925c08235c3a
St. Elmo Wilken <stelmozors@gmail.com>
2022-08-20 12:55:25 +0200

    update random init point plot

--- analyses/gd_gecko/get_ml_random_init.jl
+++ analyses/gd_gecko/get_ml_random_init.jl
@@ -99,7 +99,7 @@ gd_df = @subset _gd_df @byrow begin
     !isnan(:Kcat_std_gd)
 end 
 
-#: Plot
+#: Plot first 
 
 df = innerjoin(rd_df, gd_df, on = :KcatID)
 
@@ -107,12 +107,13 @@ xs = log10.(df[!, :Kcat_mean]./df[!, :Kcat_mean_gd])
 
 count(-0.5 .< xs .< 0.5)/length(xs)
 
-fig = Figure(
-    # resolution = (1200, 1200),
-    backgroundcolor = :transparent,
-)
+fig = Figure(resolution = (1200, 600), backgroundcolor = :transparent);
+
+ga = fig[1, 1] = GridLayout()
+gb = fig[1, 2] = GridLayout()
+
 ax = Axis(
-    fig[1,1],
+    ga[1,1],
     xlabel = "Log ratio of random start kcat to ML start kcat",
     ylabel = "Density"
 )
@@ -128,14 +129,14 @@ hidexdecorations!(ax, label = false, ticklabels = false, ticks = false)
 hideydecorations!(ax, label = false, ticklabels = false, ticks = false)
 
 Legend(
-    fig[1,1],
+    ga[1,1],
     [
         [PolyElement(color = :mediumorchid2, strokecolor = :mediumorchid2, strokewidth = 1),],
         [PolyElement(color = :tomato, strokecolor = :tomato, strokewidth = 1),],
     ],
     [
-        "Optimal random start iterate within 3\nfold of ML start optimal iterate",
-        "Optimal random start iterate not changed\nwrt ML start optimal start iterate",
+        "Optimal random start iterate\nwithin 3 fold of ML\nstart optimal iterate",
+        "Optimal random start iterate\nnot changed wrt ML start\noptimal start iterate",
     ],
     tellheight = false,
     tellwidth = false,
@@ -143,6 +144,103 @@ Legend(
     valign = :top,
     margin = (10, 10, 10, 10),
 )
+
+
+
+
+#: Plot the next dataset
+dfl = DataFrame(Condition = String[], Loss = Float64[], Iteration = Int64[])
+for dir in losses_dir
+    append!(dfl, DataFrame(CSV.File(joinpath("results", rdir, dir))))
+end
+master_ids = unique(dfl[!, :Condition])
+
+cond_minlossiter = Dict()
+for gdf in groupby(dfl, :Condition)
+    losses = gdf[!, :Loss]
+    idx = argmin(losses)
+    cond_minlossiter[gdf[idx, :Condition]] = gdf[idx, :Iteration]
+end
+
+#: load kcat data
+kbest_df = DataFrame(Condition = String[], KcatID = String[], Kcat = Float64[], Derivative = Float64[], Iteration = Float64[])
+for master_id in master_ids
+    try
+        pdirfile = master_id * "#params.csv"
+        dfp = DataFrame(CSV.File(joinpath("results", rdir, pdirfile)))
+
+        df = @subset dfp @byrow begin 
+            :Iteration == cond_minlossiter[master_id]
+        end
+
+        append!(
+            kbest_df,
+            df,
+        )
+    catch err
+        println("failed on ", master_id)
+    end
+end
+
+until_digit(x) = begin
+    if startswith(x, "WT")
+        return "WT"
+    elseif startswith(x, "pgi")
+        return "pgi"
+    elseif startswith(x, "tpi")
+        return "tpi"
+    elseif startswith(x, "pts")
+        return "pts"
+    elseif startswith(x, "sdh")
+        return "sdh"
+    else
+        throw(error("no match"))
+    end
+end
+
+gd_df = transform(
+    kbest_df,
+    :Condition => x -> until_digit.(x),
+    :KcatID => x -> last.(split.(x, "#")),
+)
+@select!(gd_df, :Kcat, :KcatID_function, :Condition_function)
+rename!(gd_df, Dict("KcatID_function" => :KcatID, "Condition_function" => :Condition))
+@transform!(gd_df, :Kcat = 1 / (3600 * 1e-6) .* :Kcat)
+
+df = combine(
+    groupby(gd_df, :KcatID),
+    :Kcat => x -> std(x)/mean(x),
+)
+
+@subset! df @byrow begin 
+    !isnan(:Kcat_function)
+end
+
+xs = df[!, :Kcat_function]
+
+ax2 = Axis(
+    gb[1,1],
+    xlabel = "Ratio of standard deviation to mean of the\nfinal kcat estimates for each enzyme across all conditions",
+    ylabel = "Density"
+)
+
+density!(ax2, xs; color= :darkturquoise, boundary=(0.0, 10.0))
+
+hidexdecorations!(ax2, label = false, ticklabels = false, ticks = false)
+hideydecorations!(ax2, label = false, ticklabels = false, ticks = false)
+
+xlims!(ax2, -0.5, 6) # sigh density wraps around :/ 
+
+for (label, layout) in zip(["A", "B"], [ga, gb])
+    Label(
+        layout[1, 1, TopLeft()],
+        label,
+        textsize = 26,
+        # font = noto_sans_bold,
+        padding = (0, 5, 5, 0),
+        halign = :right,
+    )
+end
 
 fig
 
